829ec8d99164af30b9efbdb1fcb123b1deb8e8cf
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2023-12-26 17:31:56 +0700

    add update method

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Resources\PostResource;
 
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -49,5 +51,41 @@ class PostController extends Controller
         $post = Post::find($id);
 
         return new PostResource(true, "Detail Data Post!", $post);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), array_merge(
+            ($request->hasFile("image") ? ["image" => "required|image|mimes:jpg,jpeg,png,svg,gif|max:2048"] : []),
+            [
+            "title" => "required",
+            "content" => "required",
+        ]));
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $post = Post::find($id);
+
+        if($request->hasFile("image")){
+            $image = $request->file("image");
+            $image->storeAs("/public/posts", $image->hashName());
+
+            Storage::delete("/public/posts/".basename($post->image));
+
+            $post->update([
+                "image" => $image->hashName(),
+                "title" => $request->title,
+                "content" => $request->content,
+            ]);
+        }else{
+            $post->update([
+                "title" => $request->title,
+                "content" => $request->content,
+            ]);
+        }
+
+        return new PostResource(true, "Data Post Berhasil Diubah!", $post);
     }
 }
